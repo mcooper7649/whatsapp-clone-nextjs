50c3f576f07911df07e995859620e5dca1f05a43
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-10-13 12:30:41 -0400

    test

---
+++ components/Sidebar.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+function Sidebar() {
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Header = styled.div``;
+
+const Container = styled.div``;
